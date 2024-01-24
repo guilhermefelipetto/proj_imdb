609e359356e4282e4ab9e983e5b746fb0fe9df10
guilhermefelipetto <guilherme.g.felipetto@gmail.com>
2024-01-24 18:03:40 -0300

    Adicionando o .gitignore

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,4 +1,26 @@
 -- create database proj_imdb;
 
-use proj_imdb;
+USE proj_imdb;
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL,
+    user_email VARCHAR(100) NOT NULL UNIQUE,
+    user_password VARCHAR(32) NOT NULL, -- temporario! PRECISO ENCRIPTAR A SENHA E NAO GUARDA-LA DIRETAMENTE NO BANCO!
+    user_category INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE category (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    category VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE user_category (
+	user_id INT,
+    category_id INT,
+    PRIMARY KEY (user_id, category_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
+);
 
